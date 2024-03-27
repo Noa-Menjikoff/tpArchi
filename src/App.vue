eb5fca08cf61bc62b8aa31b98e2fa463d0549b78
noa menjikoff <noa.menjikoff@gmail.com>
2024-03-27 17:24:54 +0100

    update

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,63 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
+<script>
+
+import TodoItem from './components/TodoItem.vue';
+
+let data ={
+  todos : [{text : 'Faire les courses',checked : true,id:1},{text : 'Taches',checked : false,id:2}],
+  title : 'Mes taches',
+  newItem : {text :''}
+};
+
+export default {
+
+  data(){
+    return data;
+  },
+  methods:{
+    addTodo: function(){
+      this.todos.push({text : this.tache ,checked : false});
+    },
+    removeItem: function(id){
+      for (var todo in this.todos){
+        if (this.todos[todo].id == id.id){
+          console.log(todo);
+          this.todos.splice(todo,1);
+        }
+      }
+      this.todos.splice();
+    }  
+  },
+
+  components : { TodoItem }
+  
+  }
+
+
 </script>
 
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
-  <HelloWorld msg="Vite + Vue" />
+
+  <div id="app6" class="container">
+            <h2>{{ title }}</h2>
+            <ol>
+              <TodoItem 
+                v-for="item of todos":todo="item"
+                @remove="removeItem">
+                
+              </TodoItem>
+            </ol>
+            <div class="container">
+                <h2> Ajouter un Todo</h2>
+                <input v-model ="tache" type="text"/>
+                <span class="input-group-btn">
+                    <button v-on:click ="addTodo"
+                        class="btn btn-default"
+                        type="button">
+                        Ajouter un todo
+                    </button>
+                </span>
+            </div>
+        </div>
 </template>
 
 <style scoped>
---
+++ src/components/TodoItem.vue
@@ -0,0 +1,29 @@
+<script>
+
+export default{
+    props:{
+        todo : Object
+    },
+    methods:{
+        suppr : function() {
+            this.$emit('remove', {id: this.todo.id});
+        }
+    },
+    emits : ['remove']
+}
+
+</script>
+
+<template>
+    <div class="checkbox">
+        <label>
+            <input type="checkbox"
+            v-model ="todo.checked">
+            {{ todo.text }}
+        </label>
+        <input type="button"
+            class ="btn btn-danger"
+            value="Supprimer"
+            @click="suppr">
+    </div>
+</template>
